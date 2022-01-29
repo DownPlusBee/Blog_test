eea5100f658e057b6c3b4a3300ef88b1dfddfbcf
Michael Gathwright <michael.gathwright@publix.com>
2022-01-29 17:20:03 -0500

    Delete duplicate ISortHelper interface.

--- Blog.Persistence/Helpers/ISortHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-
-namespace Blog.Persistence.Helpers
-{
-    public interface ISortHelper<T>
-    {
-        IQueryable<T> ApplySort(IQueryable<T> entities, string sortBy, string sort);
-    }
-}
